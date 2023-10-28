0b79006f6c5e2c9c8a4f9289424ce2773a042321
tekvyy <divgun.gta2@gmail.com>
2023-10-28 18:52:25 -0400

    Added constructor and getter methods for the smart contract

--- canary_contract/lib.rs
+++ canary_contract/lib.rs
@@ -21,6 +21,38 @@ mod canary_contract {
     }
 
     impl CanaryContract {
-        
+
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new(main_contract: AccountId, canary_contract: AccountId) -> Self {
+            Self {
+                admin: Self::env().caller(),
+                main_contract,
+                canary_contract,
+                total_calls: 0,
+                canary_percentage: 0,
+            }
+        }
+
+        /// Simply returns the current value of prod contract
+        #[ink(message)]
+        pub fn get_main_contract(&self) -> AccountId {
+            self.main_contract
+        }
+
+        #[ink(message)]
+        pub fn get_total_calls(&self) -> i32 {
+            self.total_calls
+        }
+
+        #[ink(message)]
+        pub fn get_canary_contract(&self) -> AccountId {
+            self.canary_contract
+        }
+
+        #[ink(message)]
+        pub fn get_canary_percentage(&self) -> i32 {
+            self.canary_percentage
+        }
     }
 }
